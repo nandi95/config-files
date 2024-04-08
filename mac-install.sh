8c491f675f4b391e0d96077f08a86d14a36bd677
Nandor Kraszlan <nandor@wooshii.com>
2024-04-08 13:48:19 +0100

    Update list of tools

--- mac-install.sh
+++ mac-install.sh
@@ -3,42 +3,51 @@
 # Ask for the administrator password upfront
 sudo -v
 # todo exit if pwd doesn't ends with /config-files
-xcode-select --install # todo - check if it exists
+#xcode-select --install # todo - check if it exists
 
 #https://github.com/Homebrew/homebrew-cask/blob/master/USAGE.md
 #install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew tap homebrew/cask-versions
+brew tap shivammathur/extensions
 
-#install packages
+# install tools
 brew install git
+brew install eza
+brew install curl
+brew install wget
+brew install zsh
+brew install nano
+brew install unar
+brew install ranger
+
+#install languages
 brew install python3
 brew install node
 brew install nano
 brew install php
-brew install curl
-brew install wget
+brew install deno
 brew install composer
-brew install act # github actions emulator
-brew install zsh
-brew install --cask docker
+
+# install apps
+brew install --cask orbstack
 brew install --cask iterm2
+brew install --cask obsidian
 brew install --cask raycast
 brew install --cask gpg-suite
 brew install --cask jetbrains-toolbox
 brew install --cask visual-studio-code
 brew install --cask slack
 brew install --cask discord
-brew install --cask zoom
-brew install --cask signal
 brew install --cask firefox-developer-edition
 brew install --cask gitkraken
 brew install --cask gitify
 brew install --cask fig
 brew install --cask google-chrome
+brew install --cask inkscape
 brew install --cask commander-one
-brew install --cask qbittorrent
+brew install --cask plex
 brew install --cask vlc
 brew install --cask spotify
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -46,11 +55,10 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # install global packages
 npm install -g tldr
 npm install -g yarn
-npm install -g git-trim
 npm install -g pnpm
 composer global require laravel/valet
 composer global require beyondcode/expose
-composer global require laravel/vapor-cli
+npm install -g git-trim
 
 # Close any open System Preferences panes, to prevent them from overriding
 # settings we’re about to change
@@ -85,3 +93,10 @@ cp ./shell/.functions /.functions
 echo "source ~/.aliases" >> /.zshrc
 echo "source ~/.functions" >> /.zshrc
 echo "run composer self-update --update-keys to set public keys for package verification"
+
+# nano test highlighting https://github.com/scopatz/nanorc
+curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
+
+# enable fingerprint instead of password for sudo commands
+#sed -i '1 i\auth       sufficient     pam_tid.so' /etc/pam.d/sudo
+echo "Update spotlight key binding"
